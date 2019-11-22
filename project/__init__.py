6c7936e242c475ec6703a77b25ef935445725ca6
mayankDhiman <mdhiman536@gmail.com>
2019-11-22 21:50:49 +0530

    pp

--- project/__init__.py
+++ project/__init__.py
@@ -8,7 +8,7 @@ app.config['SECRET_KEY'] = 'youwontguess'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('HEROKU_POSTGRESQL_BLUE_URL') or 'sqlite:///'+os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['LOG_TO_STDOUT'] = os.environ.get('LOG_TO_STDOUT')
 
