00269d2e5cb31883184116c0fb36bdbbae9f17fb
mayankDhiman <mdhiman536@gmail.com>
2019-11-26 18:50:12 +0530

    bugs

--- project/__init__.py
+++ project/__init__.py
@@ -1,20 +1,9 @@
 from flask import Flask
 from project.indexer import Database, InvertedIndex
 import os
-
 app= Flask(__name__)
 app.config['SECRET_KEY'] = 'youwontguess'
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-"""
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('HEROKU_POSTGRESQL_BLUE_URL') or 'sqlite:///'+os.path.join(basedir,'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['LOG_TO_STDOUT'] = os.environ.get('LOG_TO_STDOUT')
-"""
-
-# db = Database()
-# invertedIndex = InvertedIndex(db)
 app.static_folder = 'static'
 from project.routes import admin
 app.register_blueprint(admin)
